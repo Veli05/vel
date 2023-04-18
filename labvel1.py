e545f7863a55d4dc010d16f07597f815c55abcd3
Veli05 <129822383+Veli05@users.noreply.github.com>
2023-04-18 11:14:53 +0800

    Update labvel1.py

--- labvel1.py
+++ labvel1.py
@@ -1,7 +1,12 @@
 import socket
-import platform
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-print(socket.gethostname())
-print(socket.gethostbyname(socket.gethostname()))
-print(s.getsockname()[0])
+import uuid
+print ("What's your name?")
+name = input ()
+print ("Hello, " +name)
+hostname = socket.gethostname()
+print (hostname)
+IPaddress = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+IPaddress.connect(('77.88.8.8', 80))
+print ("IP addres of your pc: " +IPaddress.getsockname()[0])
+print ("MAC address of your pc: ", end="")
+print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1]))
