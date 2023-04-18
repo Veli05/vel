ae3f2bedf17d755ab888a60b16ea3a763a7e01fb
Veli05 <129822383+Veli05@users.noreply.github.com>
2023-04-18 21:22:26 +0800

    Delete labvel1.py

--- labvel1.py
+++
@@ -1,7 +0,0 @@
-import socket
-import platform
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-print(socket.gethostname())
-print(socket.gethostbyname(socket.gethostname()))
-print(s.getsockname()[0])
