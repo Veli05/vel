e20c1804f2ca17844cb872e604dd6114c54a44ab
Veli05 <129822383+Veli05@users.noreply.github.com>
2023-05-01 21:18:52 +0800

    Delete labvel3.py

--- labvel3.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-Template = "http://biik.ru/rasp/cg109.htm" # Запрос на сайт Биик Сибгути
-a = requests.get(Template) # получение запроса
-a.encoding="windows-1251" # кодировка
-print(a.text) # вывод на экран
